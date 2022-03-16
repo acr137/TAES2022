19291d4829803a957b63a2cd61ac0204c34cbdaa
VictorHJua <vhj1@gcloud.ua.es>
2022-03-16 11:50:53 +0100

    menu niveles mejorado: Añadido niveles bloqueados, fondo cambiado, añadido botones de ir atras en niveles y nivel1

--- Space Raiders/Assets/Nivel1.cs
+++ Space Raiders/Assets/Nivel1.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Seleccionar_nivel : MonoBehaviour
+
+public class Nivel1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,12 +16,9 @@ public class Seleccionar_nivel : MonoBehaviour
     {
         
     }
-    public void LoadNivel1() 
+
+    public void LoadMenu_niveles()
     {
-        SceneManager.LoadScene("Nivel_1");
-    }
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("menu_niveles");
     }
 }
