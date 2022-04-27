161ff636deffbce7626e51397aca737953c100e3
Robert Kazzaryan <rk30@gcloud.ua.es>
2022-04-27 17:41:47 +0200

    first commit

--- Space Raiders/Assets/Scripts/ShopMenu_Manager.cs
+++ Space Raiders/Assets/Scripts/ShopMenu_Manager.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using TMPro;
 
 public class ShopMenu_Manager : MonoBehaviour
 {
     public AudioSource clip;
 
+    /// <summary>
+    /// Texto a proyectar
+    /// </summary>
+    public TextMeshProUGUI saldoText;
+    private int saldo = 0;
+    private string nombreSaldo = "saldo";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saldo = PlayerPrefs.GetInt(nombreSaldo, 0);
+
+        saldoText.text = "Saldo: " + saldo.ToString();
+    }
+
+    public void substract(int coste)
+    {
+        int result = saldo - coste;
+        if(result < 0)
+        {
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt(nombreSaldo, result);
+        }
+    }
+
     public void goTo_MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+
     public void PlaySoundButton(){
         clip.Play();
     }
